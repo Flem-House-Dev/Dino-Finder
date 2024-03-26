92812a9484d90374339c4cfb406bd9bc0a4a5b24
Jeremy Fleming <thevisionmachine@gmail.com>
2024-03-26 07:29:01 -0500

    created separate js and css folders

---
+++ assets/js/form.js
@@ -0,0 +1,19 @@
+const submitEl = document.getElementById("submitBtn");
+const formUserName = document.getElementById('userName');
+const formTitle = document.getElementById('title');
+const formContent = document.getElementById('content');
+
+const  saveFormData =  function() {
+    const dinoForm = {
+        userName: formUserName.value,
+        title: formTitle.value,
+        content: formContent.value
+    };
+    localStorage.setItem('dinoForm', JSON.stringify(dinoForm));
+}
+
+submitEl.addEventListener('click', function(event) {
+    event.preventDefault(event);
+    saveFormData();
+    window.location.href = "blog.html";
+})
