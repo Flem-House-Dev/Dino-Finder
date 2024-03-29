a3261a2f3cc5471d207df73eb162e1d1e12501fe
Jeremy Fleming <thevisionmachine@gmail.com>
2024-03-29 06:45:20 -0500

    Reapply "adding funtionality to save multiple posts"
    This reverts commit 3326fc86b99c6024e9cc527a0e3092c7797360bd.

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,16 +3,25 @@ const formUserName = document.getElementById('userName');
 const formTitle = document.getElementById('title');
 const formContent = document.getElementById('content');
 
-const  saveFormData =  function() {
+// save array of objects to make multiple ones.
+// use for loop to render back to page, retrieve arrau. add to it, then parse and save it again
+let formArray = [];
+
+const saveFormData = function () {
     const dinoForm = {
         userName: formUserName.value,
         title: formTitle.value,
         content: formContent.value
     };
-    localStorage.setItem('dinoForm', JSON.stringify(dinoForm));
+
+    formArray = JSON.parse(localStorage.getItem('formArray'));
+    // console.log(blogData);
+
+    formArray.push(dinoForm);
+    localStorage.setItem('formArray', JSON.stringify(formArray));
 }
 
-submitEl.addEventListener('click', function(event) {
+submitEl.addEventListener('click', function (event) {
     event.preventDefault(event);
     saveFormData();
     window.location.href = "blog.html";
